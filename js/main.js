cedde503b6e0b14dba0fdc8c72f852d4bc9c493e
Иван Логинов <iv.mi.loginov@gmail.com>
2018-11-06 21:54:40 +0100

    Start of a new concept, display all tiles at first, centralized them

--- js/main.js
+++ js/main.js
@@ -2,36 +2,24 @@ $(document).ready(function(){
 
   fill_object('#top_content', create_top_content());
   fill_object('#main_content', create_main_content());
-  fill_object('#left_content', create_left_content());
-  fill_object('#center_content', create_desired_content());
-
+  fill_object('#center_content', create_tiles());
+  adjust_last_tile();
   function fill_object(which, filling){
     $(which).html(filling);
   }
 
   function create_top_content(){
     var element = '<table id="top_table" class="full_width">';
-    element += '<td class="left_content_td">';
-    element += '<table><td style="vertical-align:bottom">'
-    element += '<div id="portal_logo"><div class="door_handle"></div></div>';
-    element += '</td>';
-    element += '<td style="vertical-align:bottom">';
-    element += '<div id="portal_name">kmnata</div>';
-    element += '</td></table>';
-    element += '</td>';
     element += '<td>';
-    element += '<table style="margin-top:5px" class="full_width"><td id="room_title">';
-    element += '</td>';
-    element += '<td style="width:140px">';
-    element += '<button id="create"';
-    element += ' class="ui inverted blue button">Create</button>';
-    element += '</td></table>';
-    element += '</td>';
-    element += '<td class="right_content_td active">';
-    element += '<div class="ui inverted right icon input" style="margin-top:10px;width:90%">';
+    element += '<div class="ui inverted right icon input" style="margin-left:15px;">';
     element += '<input id="top_search" type="text" placeholder="Search...">';
     element += '<i class="search icon"></i>';
     element += '</div>';
+    element += '</td>';
+    element += '<td>';
+    element += '<button id="create" class="ui inverted blue button">Create</button>';
+    element += '</td>';
+    element += '<td class="right_content_td active">';
     element += '</td>';
     element += '</table>';
     return element;
@@ -39,132 +27,11 @@ $(document).ready(function(){
 
   function create_main_content(){
     var element = '<table id="main_table" class="full_width">';
-    element += '<td class="left_content_td" style="padding-right:0px"><div id="left_content"></div></td>';
+    element += '<td class="left_content_td"><div id="left_content"></div></td>';
     element += '<td style="vertical-align:top"><div id="center_content">';
     element += '</div><div id="center_bottom" class="bottom_control"></div></td>';
-    element += '<td class="right_content_td"><div id="tabs"></div><div id="right_content"></div>';
+    element += '<td class="right_content_td"><div id="right_content"></div>';
     element += '<div id="right_bottom" class="bottom_control"></div></td>';
-    element += '</table>';
-
-    return element;
-  }
-
-  function create_left_content(){
-    var element = '<table id="left_content_table" class="full_width">';
-    element += '<tr class="left_content_tr">';
-    element += '<td><div id="total_online"><i class="user icon"></i><span class="amount">906</span></div></td>';
-    element += '</tr>';
-    element += '<tr class="left_content_tr">';
-    element += '<td><div id="total_views"><i class="eye icon"></i><span class="amount">123K</span></div></td>';
-    element += '</tr>';
-    element += '<tr class="left_content_tr">';
-    element += '<td><div id="desired" class="navigation link chosen"><i class="heart icon"></i> desired</div></td>';
-    element += '</tr>';
-    element += '<tr class="left_content_tr">';
-    element += '<td><div id="explore" class="navigation link"><i class="building icon"></i> explore</div></td>';
-    element += '</tr>';
-    element += '</table>';
-
-    return element;
-  }
-
-  function create_desired_content(){
-    var data = [{
-                  "header": "Java scanner not waiting for user input",
-                  "tags": ["java", "java.util.scanner"],
-                  "ninjas": 33,
-                  "static": 0,
-                  "video": 0
-                },
-                {
-                  "header": "Query not working properly with LEFT JOIN (MySQL)",
-                  "tags": ["mysql", "sql", "join"],
-                  "ninjas": 22,
-                  "static": 3,
-                  "video": 2
-                },
-                {
-                  "header": "How is it that a {} block can represent a function in scala?",
-                  "tags": ["scala"],
-                  "ninjas": 22,
-                  "static": 1,
-                  "video": 1
-                },
-                {
-                  "header": "Strip Characters form URL using Javascript",
-                  "tags": ["javascript", "arrays", "split", "strip"],
-                  "ninjas": 21,
-                  "static": 2,
-                  "video": 0
-                },
-                {
-                  "header": "Expose docker in VB to windows host",
-                  "tags": ["windows", "docker", "ubuntu", "virtualbox"],
-                  "ninjas": 2,
-                  "static": 0,
-                  "video": 0
-                },
-                {
-                  "header": "Crontab (Centos7) with Python Files",
-                  "tags": ["python", "cron", "centos7", "scheduler"],
-                  "ninjas": 2,
-                  "static": 0,
-                  "video": 0
-                },
-                {
-                  "header": "Boost locale 'Conversion failed' for gettext",
-                  "tags": ["c++", "boost", "gettext", "mo"],
-                  "ninjas": 9,
-                  "static": 1,
-                  "video": 0
-                },
-                {
-                  "header": "pass image from FileReader to form input in Angular 6",
-                  "tags": ["angular", "file-upload", "angular6", "image-uploading", "angular-reactive-forms"],
-                  "ninjas": 17,
-                  "static": 2,
-                  "video": 1
-                }]
-
-      var i=0;
-      var j, tags;
-      var element = '<table class="full_width">';
-      while (i<data.length){
-        var color =
-        element += '<tr class="room_tr">';
-        element += '<td class="room_td">';
-        element += '<div class="door"';
-        element += 'style="background-color:'+get_random_rgba()+'">';
-        element += '<div class="door_handle"></div></div></td>';
-        element += '<td class="room_td">'
-        element += '<table><tr>'
-        element += '<td><div class="room_name">'+data[i].header+'</div></td>';
-        element += '</tr>';
-        element += '<tr><td>';
-        j=0;
-        tags = data[i].tags;
-        while (j<tags.length){
-          element += '<div class="tags">'+tags[j]+'</div>';
-          j++;
-        }
-        element += '</td></tr></table>';
-        element += '</td>';
-        element += '<td class="room_td">';
-        element += '<div class="brief_view">';
-        element += 'Brief view</div>';
-        element += '</tr>';
-        i++;
-      }
-      element += '</table>';
-
-      return element;
-  }
-
-  function create_explore_content(){
-    var element = '<table class="full_width">';
-    element += '<tr><td>';
-    element += '<div style="font-size:18px; color:rgba(255,255,255,0.5)">Check it later</div>';
-    element += '</td></tr>';
     element += '</table>';
 
     return element;
@@ -244,84 +111,98 @@ $(document).ready(function(){
       element += create_chat();
     }
     element += '</table>';
-    $('#center_content').html(element).attr('class','chat');
-    $('#right_content').attr('class','chat');
+    $('#right_content').html(element).attr('class','chat');
+    $('#center_content').attr('class','chat');
     element = '<div id="upload_media" class="control_button">';
     element += '<i class="paperclip icon"></i></div>';
     element += '<textarea class="input_field" placeholder="Message">';
     element += '</textarea>';
-    $('#center_bottom').html(element);
-    fill_object('#right_content', create_video_tiles());
-    fill_object('#tabs', create_filter_tabs());
+    $('#right_bottom').html(element);
+    fill_object('#center_bottom', create_topics());
     $('#desks').click();
+    adjust_last_tile();
   }
 
-  function create_video_tiles(){
-    var array = ['https://www.youtube.com/embed/5AEbq6X33A8'
-                , 'https://www.youtube.com/embed/LbG5STFZ3C0'];
-    var i=0;
-    var element = '';
-    while (i<array.length){
-      color = 'color:'+get_random_rgba();
-      element += '<div class="tile">';
-      element += '<div class="tile_user_name"><i class="bug icon" style="'+color+'">';
-      element += '</i> Ninja '+(i+1)+'</div>';
-      element += '<div class="tile_content">';
-      element += '<iframe style="width:100%;height:100%"src="'+array[i]+'"';
-      element += ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-      element += '</div></div>';
+  function adjust_last_tile(){
+    var x = $('.tile');
+    var i = x.length-1;
+    $(x[i]).css('margin-right','0px');
+    var top = $(x[i]).offset().top;
+    var q_last_row = 0;
+    var q_first_row = 0;
+    i--;
+    while ($(x[i]).offset().top==top && i>-1){
+      q_last_row++;
+      i--;
+    }
+    i = 0;
+    top = $(x[i]).offset().top;
+    i++;
+    while ($(x[i]).offset().top==top && i<x.length){
+      q_first_row++;
       i++;
     }
-
-    return element;
+    console.log(q_first_row + ' ' + q_last_row);
+    var margin = (q_first_row - q_last_row)*$(x[0]).outerWidth(true)+10;
+    $(x[x.length-1]).css('margin-right', margin);
   }
 
   function create_tiles(){
-    var i=0;
-    var j=0;
-    var element = '<div class="tile">';
-    element += '<div class="tile_user_name">Main chat</div>';
-    element += '<div class="tile_content">';
-    element += '<table class="full_width">'+create_chat()+'</table>';
-    element += '</div></div>';
-    var uniq_users = get_values(messages, 'user');
-    uniq_users = uniq_users.filter(get_unique);
-    var color;
-    while (i<uniq_users.length){
-      color = 'color:'+get_random_rgba();
-      element += '<div class="tile">';
-      element += '<div class="tile_user_name"><i class="bug icon" style="'+color+'">';
-      element += '</i> '+uniq_users[i]+'</div>';
-      element += '<div class="tile_content"><table class="full_width">';
-      j=0;
-      while (j<messages.length){
-        if (messages[j].user==uniq_users[i]){
-          element += '<tr><td>';
-          element += '<div class="user_message">'+messages[j].message+'</div>';
-          element += '</td></tr>';
-        }
-        j++;
-      }
-      element += '</table></div></div>';
-      i++;
-    }
-
-    return element;
-  }
-
-  function create_filter_tabs(){
-    var array = ['DESKS', 'CHAT'];
+    var array = [
+                  {
+                    'type': 'video'
+                  , 'src':'https://www.youtube.com/embed/5AEbq6X33A8'
+                  , 'name':'24/7 lofi hip hop radio - beats to chill/study/relax'
+                }
+                , {
+                    'type': 'video'
+                  , 'src':'https://www.youtube.com/embed/LbG5STFZ3C0'
+                  , 'name': 'Samurai Champloo episode 01/26'
+                }
+                , {
+                  'type': 'chat'
+                , 'src':'How to steal honey from bees'
+                , 'name': 'How to steal honey from bees'
+                }
+                , {
+                  'type': 'video'
+                , 'src':'https://www.youtube.com/embed/YyoKXfBQgXw'
+                , 'name': 'Tron(2010) Disc Wars (action only)'
+                }
+                , {
+                  'type': 'chat'
+                , 'src':'Making online presentation'
+                , 'name': 'Making online presentation'
+                }
+              ];
     var i=0;
     var element = '';
+    var dots = '';
     while (i<array.length){
-      element += '<div id="'+array[i].toLowerCase()+'" title="Show here '+array[i].name+'"';
-      element += ' class="tabs">';
-      element += array[i];
+      color = 'color:'+get_random_rgba();
+      element += '<div class="tile">';
+      element += '<div class="tile_content">';
+      if (array[i].type=='video'){
+        element += '<div class="topic_name"><iframe style="width:100%;height:100%"src="'+array[i].src+'"';
+        element += ' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>';
+      } else {
+        element += '<div class="topic_name" style="background-color:'+get_random_rgba()+'">';
+        element += array[i].src+'</div>';
+      }
+      dots = (array[i].name.length>27 ? '...' : '');
+      element += '<div class="tile_name" title="'+array[i].name+'">'+array[i].name.slice(0,27)+dots+'</div>';
+      element += '<div class="tile_user_name">';
+      element += 'Ninja '+(i+1)+'</div>';
+      element += '</div>';
       element += '</div>';
       i++;
     }
 
     return element;
+  }
+
+  function create_topics(){
+    return 0;
   }
 
   function get_unique(value, index, self){
@@ -369,10 +250,6 @@ $(document).ready(function(){
 
   });
 
-  $(document).on('click', '.brief_view', function(e){
-    fill_object('#right_content', '<table class="full_width chat">'+create_chat()+'</table>');
-  })
-
   $(document).on('click', '#create', function(e){
     goto_room(1);
   });
@@ -389,40 +266,24 @@ $(document).ready(function(){
     var center;
     var right;
     var c_bot, r_bot;
-    if (id == 'desks'){
-      center = '<table class="full_width">';
-      center += create_chat();
-      center += '</table>';
-      right = create_video_tiles();
-      r_bot = '';
-      c_bot = '<div id="upload_media" class="control_button">';
-      c_bot += '<i class="paperclip icon"></i></div>';
-      c_bot += '<textarea class="input_field" placeholder="Message">';
-      c_bot += '</textarea>';
-    } else {
-      center = create_video_tiles();
-      right = '<table class="full_width">';
-      right += create_chat();
-      right += '</table>';
-      r_bot = '<div id="upload_media" class="control_button">';
-      r_bot += '<i class="paperclip icon"></i></div>';
-      r_bot += '<textarea class="input_field" placeholder="Message">';
-      r_bot += '</textarea>';
-      c_bot = '';
+    center = create_tiles();
+    right = '<table class="full_width">';
+    right += create_chat();
+    right += '</table>';
+    r_bot = '<div id="upload_media" class="control_button">';
+    r_bot += '<i class="paperclip icon"></i></div>';
+    r_bot += '<textarea class="input_field" placeholder="Message">';
+    r_bot += '</textarea>';
+    c_bot = '';
 
-    }
     fill_object('#center_content', center);
     fill_object('#right_content', right);
     fill_object('#center_bottom', c_bot);
     fill_object('#right_bottom', r_bot)
   })
 
-  $(document).on('click', '.tile_content', function(e){
-    if ($('.tabs.chosen').html()=='DESKS'){
-      $('#chat').click();
-    }
-
-    $('#center_content').html($(this).html());
+  $(document).on('click', '.tile', function(e){
+    goto_room(0, $(this).find('.tile_name').attr('title'));
   })
 
 })
